f1c0b75533d3dbf304ea82359b4f659e61ccb586
lizhiliang <lizhiliang@zhixueyun.com>
2019-08-19 11:03:57 +0800

    lzl-0819

---
+++ canvas/html2Canvas/index.js
@@ -0,0 +1,7 @@
+console.log('aksdhgfags');
+// const html2canvas = require('html2canvas');
+// import html2canvas from 'html2canvas';
+
+html2canvas(document.querySelector("#capture")).then(canvas => {
+    document.body.appendChild(canvas)
+});
